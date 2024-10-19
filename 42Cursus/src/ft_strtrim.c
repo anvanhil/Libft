f908883f5b968c03f7788eff3488cf21185f8ba6
anvanhil <92312837+anvanhil@users.noreply.github.com>
2024-10-19 11:43:36 +0200

    Add files via upload

--- 42Cursus/src/ft_strtrim.c
+++ 42Cursus/src/ft_strtrim.c
@@ -6,45 +6,48 @@
 /*   By: placombe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 17:51:37 by placombe          #+#    #+#             */
-/*   Updated: 2024/10/19 11:17:14 by placombe         ###   ########.fr       */
+/*   Updated: 2024/10/19 11:43:08 by placombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../libft.h"
 
-int check_set(char c, const char *set)
+int	check_set(char c, const char *set)
 {
-    int j = 0;
-    while (set[j] != '\0')
-    {
-        if (c == set[j])
-            return (1);
-        j++;
-    }
-    return (0);
+	int	j;
+
+	j = 0;
+	while (set[j] != '\0')
+	{
+		if (c == set[j])
+			return (1);
+		j++;
+	}
+	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    int i;
-    int start;
-    int end;
-    char *trim;
+	int		i;
+	int		start;
+	int		end;
+	char	*trim;
 
-    start = 0;
+	start = 0;
 	end = ft_strlen(s1) - 1;
-    while (s1[start] && check_set(s1[start], set))
-        start++;
-    while (end > start && check_set(s1[end], set))
-        end--;    
-    trim = (char *)malloc(sizeof(char) * (end - start + 2)); 
-    if (!trim)
-        return (NULL);
-    i = 0;
-    while (start <= end)
-        trim[i++] = s1[start++];
-    trim[i] = '\0';
-    return (trim);
+	while (s1[start] && check_set(s1[start], set))
+		start++;
+	while (end > start && check_set(s1[end], set))
+		end--;
+	trim = (char *)malloc(sizeof(char) * (end - start + 2));
+	if (!trim)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+		trim[i++] = s1[start++];
+	trim[i] = '\0';
+	return (trim);
 }
+/*
 #include<stdio.h>
 int main()
 {
@@ -52,4 +55,4 @@ int main()
     char set[] = "S";
     printf("Resultat : '%s'\n", ft_strtrim(s, set));
     return 0;
-}
+}*/
