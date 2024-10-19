0c0d0f3004c5a4712878226b37ccce4212d366a3
anvanhil <92312837+anvanhil@users.noreply.github.com>
2024-10-19 11:18:06 +0200

    Add files via upload

--- 42Cursus/src/ft_strtrim.c
+++ 42Cursus/src/ft_strtrim.c
@@ -6,30 +6,50 @@
 /*   By: placombe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 17:51:37 by placombe          #+#    #+#             */
-/*   Updated: 2024/10/18 18:01:29 by placombe         ###   ########.fr       */
+/*   Updated: 2024/10/19 11:17:14 by placombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "../libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+int check_set(char c, const char *set)
 {
-	int		i;
-	int		len;
-	char	*trim;
+    int j = 0;
+    while (set[j] != '\0')
+    {
+        if (c == set[j])
+            return (1);
+        j++;
+    }
+    return (0);
+}
 
-	i = 0;
-	len = ft_strlen(s1);
-	trim = (char *)malloc((sizeof(char ) * len) + 1);
-	while (s1[i] != '\0')
-	{
-		if (s1[i] == set)
-			i++;
-		else
-		{
-			trim[i] = s1[i];
-			i++;
-		}
-	}
-	return (trim);
-} 
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int i;
+    int start;
+    int end;
+    char *trim;
+
+    start = 0;
+	end = ft_strlen(s1) - 1;
+    while (s1[start] && check_set(s1[start], set))
+        start++;
+    while (end > start && check_set(s1[end], set))
+        end--;    
+    trim = (char *)malloc(sizeof(char) * (end - start + 2)); 
+    if (!trim)
+        return (NULL);
+    i = 0;
+    while (start <= end)
+        trim[i++] = s1[start++];
+    trim[i] = '\0';
+    return (trim);
+}
+#include<stdio.h>
+int main()
+{
+    char s[] = "S SalutS S";
+    char set[] = "S";
+    printf("Resultat : '%s'\n", ft_strtrim(s, set));
+    return 0;
+}
